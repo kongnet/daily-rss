cdfe07ecd158c0e63fa4d76851013dc2c7c90031
aichitutu <1217033134@qq.com>
2023-01-17 20:03:04 +0800

    add md

--- job/rss2md.js
+++ job/rss2md.js
@@ -19,7 +19,7 @@ async function getBing() {
     })
     const item = JSON.parse(rst)?.rss?.channel?.item[0]
     if (item) {
-        return {title:item.title, pic:descToUrl(item?.description?._cdata)}
+        return {title:item.title._cdata, pic:descToUrl(item?.description?._cdata)}
     }
     return {}
 }
@@ -43,6 +43,10 @@ async function start(obj) {
     const monthDir = `${fileDir}${monTime}`
     if (!hFs.mkdir(monthDir)) return false
 
+    const monReadmePath = `${monthDir}/README.md`
+    if (!fs.existsSync(monReadmePath)){
+        hFs.saveFile(monReadmePath,'#')
+    }
     const filePath = `${monthDir}/${dayTime}.md`
     if (!fs.existsSync(filePath)) {
         const mds = await genMD(obj)
@@ -55,24 +59,18 @@ async function start(obj) {
 
 function setSideBar(files) {
     let result = []
-    let arr = []
-    for (const file in files) {
-        arr.push(+file)
-    }
+    let arr = Object.keys(files)
     arr.sort()
     arr.reverse()
     arr.forEach(a=> {
         if(a<200000) return
         const monStr = `${Math.floor(a/100)}-${a%100<10?'0'+a%100:a%100}`
         result.push(`- [${monStr}](docs/${monStr}/)`)
-        let arr2 = []
-        for (const file2 in files[a]){
-            arr2.push(+file2)
-        }
+        let arr2 = Object.keys(files[a])
         arr2.sort()
         arr2.reverse()
         arr2.forEach(v => {
-            if (v<200000) return
+            if (v<200000 || v==='README') return
             const dayStr = `${monStr}-${v%100<10?'0'+v%100:v%100}`
             result.push(`    - [${dayStr}](docs/${monStr}/${dayStr}.md)`)
         })
@@ -87,19 +85,35 @@ async function genMD(obj) {
     result.push(`# 每日新闻 ${$.now().format('YYYY-MM-DD')}`)
     const bing = await getBing()
     result.push(`![](${bing.pic})`)
-    result.push(`> ${bing?.title?._cdata}  `)
+    result.push(`> ${bing?.title}  `)
     result.push(' ')
-    result.push(`[历史上的今天](today-html/${$.now().format('MMDD')}.html)`)
+    result.push(`## [历史上的今天](today-html/${$.now().format('MMDD')}.html)`)
     for (const source in obj) {
         result.push(`## ${commonName[source]?commonName[source]:source}`)
         obj[source].forEach((v,i) => {
             result.push(`### ${i+1}. ${v.title}`)
-            result.push(`> ${v?.desc?._cdata.replace(/(<([^>]+)>)/gi, '').replace(/[\r\n]/g, '  \r\n> ')}`)
+            result.push(setDesc(v.desc))
             result.push(`[查看详情](${v.link})`)
         })
     }
     return result.join('\n')
 }
+
+function setDesc(desc) {
+    try{
+        let rst = `> ${desc.replace(/(<([^>]+)>)/gi, '').replace(/[\r\n]/g, '  \r\n> ').replaceAll('#','')}`
+        if (!rst) {
+            return ''
+        }
+        // if (rst.length > 350) {
+        //     rst = rst.slice(0,350)+'...'
+        // }
+        return rst
+    }catch (e) {
+        console.log(e.message)
+    }
+}
+
 
 async function readFilePath (dir) {
     const files = (await hFs.readdir(dir)) || []
